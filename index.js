8f5e448138158ea2c607acb0d7545b865d75eefb
fauzihazim <fauzihazim28@gmail.com>
2026-01-07 18:32:08 +0700

    harusnya error

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ app.get('/greet', (req, res) => {
 // Start server only if not in test mode
 if (require.main === module) { 
   const PORT = 3000;
-  
-  
   app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
   });
